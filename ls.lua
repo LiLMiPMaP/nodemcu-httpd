e8f641303950c15ac63a88db590cc795814890be
Lionello Lunesu <lio+git@lunesu.com>
2017-04-01 13:39:42 +0800

    Ignore built .gz files

--- ls.lua
+++
@@ -1,7 +0,0 @@
-return function(connection, payload)
-  connection:send("HTTP/1.0 200 OK\r\n\r\n")
-  for k,v in pairs(file.list()) do
-    connection:send(k .. "\t" .. v .. "\n")
-  end
-  connection:close()
-end
